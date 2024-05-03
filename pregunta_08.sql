3f1c8488cc6b27d9798e90605cd4643c19ba6cc1
Carturo8 <caarojasbo@unal.edu.co>
2024-05-02 19:46:25 -0500

    sol_lab8_intento2

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,7 +41,6 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT YEAR(c23) AS "YEAR", AVG(c21) AS "avg(c21)"
+SELECT strftime('%Y', c23), avg(c21)
 FROM tbl2
-GROUP BY YEAR(c23)
-ORDER BY YEAR(c23);
+GROUP BY strftime('%Y', c23);
